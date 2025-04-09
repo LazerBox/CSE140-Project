7e5ac38dc645b5a18811f51589775c8b2d185385
Ivan Vargas <vargasivan967@gmail.com>
2025-04-09 16:55:05 -0700

    Added main.py file, and the two test txt files

---
+++ main.py
@@ -0,0 +1,43 @@
+'''
+Welcome to our file
+'''
+#Globals
+total_clock_cycles = 0
+
+
+#Functions
+def fetch():
+    return 0
+
+def decode():
+    return 0
+
+def execute():
+    return 0
+
+def mem():
+    return 0
+
+def writeBack():
+    total_clock_cycles += 1
+    return 0
+
+
+'''Place Actionable Code Below'''
+def main():
+    print("Hello from main!")
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+'''
+Plan to finish
+___
+___
+___
+___
+___
+'''
