b40aa2b4317681d13c537eea50b51a6741987997
Ivan Vargas <vargasivan967@gmail.com>
2025-04-10 01:41:51 -0700

    Updated

--- main.py
+++ main.py
@@ -3,29 +3,71 @@ Welcome to our file
 '''
 #Globals
 total_clock_cycles = 0
+pc = 0
+branch_target = 0
+alu_zero = 0
+next_pc = 0
 
+d_mem = [0 for _ in range(32)]
+rf = [0 for _ in range(32)]
+
+instructions = []
 
 #Functions
+def read_file(filename):
+    with open(filename, "r") as f:
+        instructions = [line.strip() for line in f if line.strip()]
+
+
 def fetch():
-    return 0
+    index = pc // 4
+
+    if index >= len(instructions):
+        return None
+
+    instruction = instructions[index]
+
+    next
+
+    return instruction
 
 def decode():
     return 0
 
-def execute():
+def execute(alu_ctrk):
     return 0
 
-def mem():
+def mem(address, lw, sw, data):
+
+
+
     return 0
 
-def writeBack():
+def writeBack(results, data):
+
+
+
     total_clock_cycles += 1
-    return 0
 
 
 '''Place Actionable Code Below'''
 def main():
-    print("Hello from main!")
+    #file_name = input("Enter the program file name to run:\n")
+    read_file("sample_part1.txt")
+
+
+    while instructions:
+
+        instruction = fetch()
+
+        decoded = decode(instruction)
+
+
+
+
+
+        print(total_clock_cycles)
+
 
 if __name__ == "__main__":
     main()
